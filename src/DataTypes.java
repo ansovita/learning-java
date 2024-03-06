b52199c49b958a5f81912a08f235dd1a950cc41a
ansovita <tatjana.ansovica@gmail.com>
2024-03-06 17:20:33 +0200

    [feature/data_tapes]: Add DataTypes class

--- src/DataTypes.java
+++ src/DataTypes.java
@@ -1,17 +1,16 @@
 public class DataTypes {
     public static void main(String[] args) {
         //First comment
-          /* Hello World
-        I am working on this file
-        Please do not hesitate to ask me
-        If you have a questions!
+
+        /*
+        Tra  ta ta ta ta ta ta
          */
 
-        // Variables: int
+        //Variables: int
         int numberOfStudents = 30;
-        int maxStore = 100;
+        int maxScore = 100;
         System.out.println(numberOfStudents);
-        System.out.println(maxStore);
+        System.out.println(maxScore);
 
         //Variables: double
         double temperature = 3.4;
@@ -19,7 +18,6 @@ public class DataTypes {
         System.out.println(temperature);
         System.out.println(price);
 
-        //Variables: float
         float pi = 3.14f;
         float radius = 5.5f;
         float area = pi * radius * radius;
@@ -27,9 +25,35 @@ public class DataTypes {
 
         //Casting
         int areaInteger = (int) area;
+        System.out.println(area);
+        System.out.println(areaInteger);
         System.out.println(Math.round(area));
 
         //Char
         char symbol = '$';
+        System.out.println(symbol);
+
+        //Exercise:
+        char n = 78; //N
+        System.out.println(n);
+
+        char t = 84; //T
+        System.out.println(t);
+        char a = 65; //A
+        System.out.println(a);
+        System.out.println(t);
+        char j = 74; //J
+        System.out.println(j);
+        System.out.println(a);
+        System.out.println(n);
+        System.out.println(a);
+
+        //Boolean is / has
+        boolean isRaining = false;
+        boolean hasPassedExercise = true;
+        boolean isSammer = false;
+        System.out.println(isRaining);
+        System.out.println(isSammer);
+        System.out.println(hasPassedExercise);
     }
 }
